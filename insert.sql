4f6e42bcea94af8713bec5b65e0733f956747841
Aditya_Patil <69051515+Adityaaddy16@users.noreply.github.com>
2023-02-18 13:21:45 +0530

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO participants (book_id, venue_id, booked_date, participant_name, participant_email, participant_phone)
+VALUES 
+(200, 1008, '2023-02-10', 'Ashish Aggarwal', 'ashish.aggarwal@gmail.com', 9820345680),
+(201, 1001, '2023-02-05', 'Sumit Kumar', 'sumit.1291@gmail.com', 9236701267),
+(202, 1005, '2023-01-20', 'Peeyush Bansal', 'peeyush_bansal20@gmail.com', 9178026731),
+(203, 1003, '2023-01-12', 'Shikha Sinha', 'shikha1990@gmail.com', 9812087410),
+(204, 1003, '2023-01-21', 'Harshika Chaturvedi', 'harshika_chaturvedi@gmail.com', 9207417841),
+(205, 1002, '2023-01-22', 'Supriya Aggarhari', 'supriya_1520@gmail.com', 9340928637),
+(206, 1004, '2023-02-12', 'Prateek Mathur', 'prateek.mathur@gmail.com', 9271648910),
+(207, 1005, '2023-02-10', 'Himanshu Singh', 'himanshu1989@gmail.com', 9517810946),
+(208, 1006, '2023-02-15', 'Shuchita Jain', 'shuchi_jain1991@gmail.com', 9104826121),
+(209, 1008, '2023-01-31', 'Rajeev Saxena', 'rajeev_saxena@gmail.com', 9654126718),
+(210, 1007, '2023-02-07', 'Navin Kumar', 'navin_kumar_19@gmail.com', 9540623913),
+(211, 1001, '2023-02-02', 'Manish Srivastava', 'manish_sri_17@gmail.com', 9271849011),
+(212, 1003, '2023-01-18', 'Nikhil Arora', 'nikhil.arora@gmail.com', 9983667102),
+(213, 1004, '2023-01-28', 'Aman Gupta', 'aman_gupta8@gmail.com', 9836167321),
+(214, 1005, '2023-02-17', 'Harsh Gujral', 'harsh.gujral@gmail.com', 9319287661),
+(215, 1007, '2023-02-13', 'Kulpreet Singh', 'kulpreet_singh@gmail.com', 9827649174),
+(216, 1002, '2023-02-17', 'Naseem Khan', 'nassem_khan@gmail.com', 9671535418),
+(217, 1008, '2023-02-02', 'Nandini Kapoor', 'nandini_kapoor12@gmail.com', 9180763892),
+(218, 1003, '2023-01-08', 'Pooja Gupta', 'pooja_g1408@gmail.com', 9401987364);
